d102f3f324da27a837ae85cc0b0c0e7b9ea907e0
Rafael Eich da Silva <reseich@gmail.com>
2021-12-04 12:02:12 -0400

    config map

--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
-import "net/http"
 
-func main(){
-    http.HandleFunc("/", Hello)
-    http.ListenAndServe(":80", nil)
+import (
+	"net/http"
+	"os"
+	"fmt"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("<h1>Hello!!!</h1>"))
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w,"Hello I'm %s. I'm %s", name ,age)
 }
